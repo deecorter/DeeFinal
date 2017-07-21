82eaaecdac7d83bfcba698035cc90127ad9e9763
Ehsanul Dodul <ehsanul.dodul@gmail.com>
2017-07-20 20:38:13 -0400

    no message

--- src/Utils/WdFuntion.java
+++ src/Utils/WdFuntion.java
@@ -1,5 +1,26 @@
 package Utils;
 
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+
 public class WdFuntion {
 
+	public static WebDriver driver;
+	public static Logger App_log= Logger.getLogger("Golog");
+	
+	public void typeByXpath(){
+		
+	};
+	
+	public void typeByName(){
+		
+	};
+	
+	public void typeById(){
+		
+	};
+	
+	public void clickByXpath(){
+		
+	};
 }
