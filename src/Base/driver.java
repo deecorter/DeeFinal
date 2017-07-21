8385e240238e4f974f46fbe8dbfbf40512ee6d8e
Ehsanul Dodul <ehsanul.dodul@gmail.com>
2017-07-20 20:39:21 -0400

    no message

--- src/Base/driver.java
+++ src/Base/driver.java
@@ -1,5 +1,22 @@
 package Base;
 
-public class driver {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
 
+import Utils.WdFuntion;
+
+public class driver extends WdFuntion {
+
+	@BeforeTest
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Jar File\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://www.ralphlauren.com/");
+		driver.manage().window().maximize();
+
+	
+
+	}	
 }
