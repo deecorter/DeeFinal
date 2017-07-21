4c7be606d4eb3aea537c141b339b5d23e892a106
Ehsanul Dodul <ehsanul.dodul@gmail.com>
2017-07-20 21:15:03 -0400

    no message

--- src/Locator/rlLocators.java
+++ src/Locator/rlLocators.java
@@ -2,4 +2,14 @@ package Locator;
 
 public class rlLocators {
 
+	//clicksByXpath
+	public String Rlpop= ".//*[@id='1825368_ltBoxMap']/area[1]";
+	public String Rlregister= ".//*[@id='accountsel']/a";
+	public String Rlmen= ".//*[@id='global-nav']/li[1]/a/span";
+	
+	//typeByXpath
+	public String Rlemail= ".//*[@id='emailAddExmpl']"; 
+	public String Rlpass= ".//*[@id='passwrd']";
+	public String Rlrepass= ".//*[@id='confPasswrd']";
+	
 }
--- src/Value/rlValue.java
+++ src/Value/rlValue.java
@@ -1,5 +1,9 @@
 package Value;
 
 public class rlValue {
+	
+	public String Rlemailvalue= "deecorter@gmail.com";
+	public String Rlpassvalue= "samethingover3";
+	public String Rlpassconf= "samethingover3";
 
 }
