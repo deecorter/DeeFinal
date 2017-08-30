0afcad534721ec593aaecb08bdda1154656024fd
Ehsanul Dodul <ehsanul.dodul@gmail.com>
2017-08-29 22:17:25 -0400

    no message

--- src/Utils/WdFuntion.java
+++ src/Utils/WdFuntion.java
@@ -1,6 +1,7 @@
 package Utils;
 
 import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class WdFuntion {
@@ -8,19 +9,23 @@ public class WdFuntion {
 	public static WebDriver driver;
 	public static Logger App_log= Logger.getLogger("Golog");
 	
-	public void typeByXpath(){
+	public void typeByXpath(String rlLocators){
+		
 		
 	};
 	
-	public void typeByName(){
+	public void typeByXpath(String rlLocators, String rlValue){
+		driver.findElement(By.xpath(rlLocators)).clear();
+		driver.findElement(By.xpath(rlLocators)).sendKeys(rlValue);
 		
 	};
 	
-	public void typeById(){
+	public void clickById(String rlLocators){
+		driver.findElement(By.id(rlLocators));
 		
 	};
 	
-	public void clickByXpath(){
-		
+	public void clickByXpath(String rlLocators){
+		driver.findElement(By.xpath(rlLocators)).click();
 	};
 }
