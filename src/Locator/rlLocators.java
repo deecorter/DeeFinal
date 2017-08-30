f940fe33ea8205b0f98ba8b2b870a2513c6fa133
Ehsanul Dodul <ehsanul.dodul@gmail.com>
2017-08-29 22:14:36 -0400

    no message

--- src/Locator/rlLocators.java
+++ src/Locator/rlLocators.java
@@ -7,10 +7,22 @@ public class rlLocators {
 	public String Rlregister= ".//*[@id='accountsel']/a";
 	public String Rlcreate= ".//*[@id='signUpButton']";
 	public String Rlmen= ".//*[@id='global-nav']/li[1]/a/span";
+	public String RlNewArv= ".//*[@id='left-nav']/div/ul[1]/li[1]/a";
+	public String RlJacket= ".//*[@id='product-134849096']/dl/dt/a[1]/img";
+	public String RlsizeL= ".//*[@id='size-swatches']/li[4]";
+	public String RlActiveWear= ".//*[@id='left-nav']/div/ul[1]/li[12]/a";
+	public String RlFleece= ".//*[@id='product-70475996']/dl/dt/a[1]/img";
+	public String Rlcolor= ".//*[@id='color-swatches']/li[3]/img";
+	public String RlFleeSize= ".//*[@id='size-swatches']/li[5]";
 	
 	//typeByXpath
 	public String Rlemail= ".//*[@id='emailAddExmpl']"; 
 	public String Rlpass= ".//*[@id='passwrd']";
 	public String Rlrepass= ".//*[@id='confPasswrd']";
+	
+	//clickById
+	public String Rladd2cart= "//select[@id='addToCart']";
+			
+	
 	
 }
